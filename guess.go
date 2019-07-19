a366ca20ca993748a07a993a04df3f5b54331135
Max G <maxdotdotg@gmail.com>
2019-07-18 21:14:49 -0400

    go fmt on guess.go

--- guess.go
+++ guess.go
@@ -7,7 +7,7 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
-    "os"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -17,33 +17,33 @@ func main() {
 	seconds := time.Now().Unix()
 	rand.Seed(seconds)
 	target := rand.Intn(100) + 1
-    //guesses := 10
+	//guesses := 10
 
 	fmt.Println("I've picked a number between 1 and 100.")
 	fmt.Println("guess the number!")
 	fmt.Println(target)
 
-    reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-    fmt.Print("what number do you think it is? ")
-    input, err := reader.ReadString('\n')
-    if err != nil {
-        log.Fatal(err)
-    }
+	fmt.Print("what number do you think it is? ")
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    input = strings.TrimSpace(input)
-    guess, err := strconv.Atoi(input)
-    if err != nil {
-        log.Fatal(err)
-    }
+	input = strings.TrimSpace(input)
+	guess, err := strconv.Atoi(input)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    if guess < target {
-        fmt.Println("Oop! guess higher!")
-        //fmt.Println("you have", guesses, "left")
-    } else if guess > target {
-        fmt.Println("Oop! guess lower!")
-        //fmt.Println("you have", guesses, "left")
-    } else {
-        fmt.Println("you got it!")
-    }
+	if guess < target {
+		fmt.Println("Oop! guess higher!")
+		//fmt.Println("you have", guesses, "left")
+	} else if guess > target {
+		fmt.Println("Oop! guess lower!")
+		//fmt.Println("you have", guesses, "left")
+	} else {
+		fmt.Println("you got it!")
+	}
 }
