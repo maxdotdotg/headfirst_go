291f60d020443af31df3793efd4b2a031087320c
Max G <maxdotdotg@gmail.com>
2019-07-27 12:20:56 -0400

    rewrite func1 to use functions

--- func1.go
+++ func1.go
@@ -3,19 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	var width, height, area float64
-	width = 4.2
-	height = 3.0
-	area = width * height
-	fmt.Printf("%0.2f liters of paint needed\n", area/10.0)
-	// resultString := fmt.Sprintf("about 1/3: %0.2f\n", 1.0/3.0)
-	// fmt.Println(resultString)
-
-	width = 5.2
-	height = 3.5
-	area = width * height
-	fmt.Printf("%.2f liters needed\n", area/10.0)
-
+    paintNeeded(4.2, 3.0)
+    paintNeeded(5.2, 3.5)
+    paintNeeded(5.0, 3.3)
 }
 
-//func area(width,height)
+func paintNeeded(width float64, height float64) {
+    area := width * height
+    fmt.Printf("%.2f liters needed\n", area/10.0)
+}
