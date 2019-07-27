596fbe150b7753dbdf38c038e85a2df693babc5e
Max G <maxdotdotg@gmail.com>
2019-07-27 12:09:46 -0400

    formatting with paint calculator

--- func1.go
+++ func1.go
@@ -7,9 +7,15 @@ func main() {
 	width = 4.2
 	height = 3.0
 	area = width * height
-	fmt.Printf("%0.2f", area/10.0, "liters of paint\n")
-	resultString := fmt.Sprintf("about 1/3: %0.2f\n", 1.0/3.0)
-	fmt.Println(resultString)
+	fmt.Printf("%0.2f liters of paint needed\n", area/10.0)
+	// resultString := fmt.Sprintf("about 1/3: %0.2f\n", 1.0/3.0)
+	// fmt.Println(resultString)
+
+	width = 5.2
+	height = 3.5
+	area = width * height
+	fmt.Printf("%.2f liters needed\n", area/10.0)
+
 }
 
 //func area(width,height)
