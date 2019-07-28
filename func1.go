2d2d43797888b75f7532fc6ff2eccb53de53ffde
Max G <maxdotdotg@gmail.com>
2019-07-28 10:32:45 -0400

    error handling

--- func1.go
+++ func1.go
@@ -1,21 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	var amount, total float64
-	amount = paintNeeded(4.2, 3.0)
+	var err error
+	amount, err = paintNeeded(4.2, 3.0)
 	fmt.Printf("%.2f liters needed\n", amount)
 	total += amount
 
-	amount = paintNeeded(5.2, 3.5)
+	amount, err = paintNeeded(5.2, 3.5)
 	fmt.Printf("%.2f liters needed\n", amount)
 	total += amount
 
-	amount = paintNeeded(5.0, 3.3)
+	amount, err = paintNeeded(5.0, 3.3)
 	fmt.Printf("%.2f liters needed\n", amount)
 	total += amount
 
+	amount, err = paintNeeded(5.0, -3.0)
+	if err != nil {
+		fmt.Println(err)
+	}
 	fmt.Printf("Total: %0.2f liters\n", total)
 
 }
@@ -25,7 +32,15 @@ func main() {
 // return some_stuff
 // }
 
-func paintNeeded(width float64, height float64) float64 {
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("width of %0.2f is invalid", width)
+	}
+
+	if height < 0 {
+		return 0, fmt.Errorf("height of %0.2f is invalid", height)
+	}
+
 	area := width * height
-	return area / 10.0
+	return area / 10.0, nil
 }
