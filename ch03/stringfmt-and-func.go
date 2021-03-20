6d5c064a2c75dcf108e553b3f96d4e44cf532ff7
Max G <maxdotdotg@gmail.com>
2021-03-20 13:12:48 -0400

    ch03 - refactor stringfmt-and-func.go for error handling

--- ch03/stringfmt-and-func.go
+++ ch03/stringfmt-and-func.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 func main() {
@@ -30,16 +31,38 @@ func main() {
 		fmt.Printf("%.2f liters of paint needed\n", area/10)
 	*/
 
-	fmt.Println(litersOfPaint(4.2, 3.0))
-	fmt.Println(litersOfPaint(5.2, 3.5))
+	amount, err := paintNeeded(4.2, 3.0)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%.2f liters of paint needed\n", amount)
+
+	amount, err = paintNeeded(-4.2, 3.0)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%.2f liters of paint needed\n", amount)
 }
 
-func litersOfPaint(width float64, height float64) string {
+func paintNeeded(width float64, height float64) (float64, error) {
 	// function definition!
 	// define required args with types, and define return type
+	if width < 0 {
+		return 0, fmt.Errorf("width can't be negative, you entered %f", width)
+	}
+
+	if height < 0 {
+		return 0, fmt.Errorf("height can't be negative, you entered %f", height)
+	}
+
 	area := width * height
 
-	// fmt.Sprintf _returns_ a formatted string instead
-	// of printing to stdout
-	return fmt.Sprintf("%.2f liters of paint needed", area/10)
+	/*
+		// fmt.Sprintf _returns_ a formatted string instead
+		// of printing to stdout
+		return fmt.Sprintf("%.2f liters of paint needed", area/10)
+	*/
+	return area / 10.0, nil
+
 }
