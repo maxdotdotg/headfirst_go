f8ce09b41a4c386567069fd35e89352cbd753059
Max G <maxdotdotg@gmail.com>
2021-04-17 13:32:31 -0400

    ch10 - add getter methods and validation to setter methods in geo package

--- ch08/geo/geo.go
+++ ch08/geo/geo.go
@@ -1,8 +1,10 @@
 package geo
 
+import "errors"
+
 type Coordinates struct {
-	Latitude  float64
-	Longitude float64
+	latitude  float64
+	longitude float64
 }
 
 type Landmark struct {
@@ -10,9 +12,27 @@ type Landmark struct {
 	Coordinates
 }
 
-func (c *Coordinates) SetLatitude(latitude float64) {
-	c.Latitude = latitude
+// setter methods
+func (c *Coordinates) SetLatitude(latitude float64) error {
+	if latitude > 90 || latitude < -90 {
+		return errors.New("invalid latitude")
+	}
+	c.latitude = latitude
+	return nil
 }
-func (c *Coordinates) SetLongitude(longitude float64) {
-	c.Longitude = longitude
+func (c *Coordinates) SetLongitude(longitude float64) error {
+	if longitude > 180 || longitude < -180 {
+		return errors.New("invalid longitude")
+	}
+	c.longitude = longitude
+	return nil
+}
+
+// getter methods
+func (c *Coordinates) Latitude() float64 {
+	return c.latitude
+}
+
+func (c *Coordinates) Longitude() float64 {
+	return c.longitude
 }
--- ch10/more_receivers.go
+++ ch10/more_receivers.go
@@ -3,11 +3,27 @@ package main
 import (
 	"fmt"
 	"geo"
+	"log"
 )
 
 func main() {
 	var coordinates geo.Coordinates
-	coordinates.SetLatitude(37.42)
-	coordinates.SetLongitude(-122.08)
+	err := coordinates.SetLatitude(37.42)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = coordinates.SetLongitude(-122.08)
+	if err != nil {
+		log.Fatal(err)
+	}
 	fmt.Println(coordinates)
+
+	// this breaks because of the validation
+	// in the setter method
+	coords2 := geo.Coordinates{}
+	err = coords2.SetLongitude(200)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
