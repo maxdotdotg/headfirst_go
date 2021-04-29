b4562e797b4d64d480c066b577ea7a7caeca5479
Max G <maxdotdotg@gmail.com>
2021-04-28 20:44:37 -0400

    ch12 - sum.go isn't working, scanner returns err, even though err == nil ```bash ➜  ch12 git:(main) ✗ GO111MODULE=off go run sum.go data.txt opening data.txt Closing file [20.25 5 10.5 15] scanner err: <nil> Sum: 0.00% ```

---
+++ ch12/sum.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func OpenFile(fileName string) (*os.File, error) {
+	fmt.Println("opening", fileName)
+	return os.Open(fileName)
+}
+
+func CloseFile(file *os.File) {
+	fmt.Println("Closing file")
+	file.Close()
+}
+
+func GetFloats(fileName string) ([]float64, error) {
+	var numbers []float64
+	file, err := OpenFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		number, err := strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return nil, err
+		}
+		numbers = append(numbers, number)
+	}
+	CloseFile(file)
+	fmt.Println(numbers)
+	if scanner.Err != nil {
+		fmt.Println("scanner err:", scanner.Err())
+		return nil, scanner.Err()
+	}
+	return numbers, nil
+}
+
+func main() {
+	numbers, err := GetFloats(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	var sum float64 = 0
+	for _, number := range numbers {
+		sum += number
+	}
+
+	fmt.Printf("Sum: %0.2f", sum)
+}
